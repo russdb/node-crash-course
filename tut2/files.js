e85a1a9fd54f557d0b3ee762b777253fbbd078be
russdb <drussell81+github@gmailcom>
2023-02-25 12:47:35 -0800

    added files back in

--- tut2/files.js
+++ tut2/files.js
@@ -5,5 +5,16 @@ fs.readFile('./docs/blog1.txt', (err, data) => {
   if (err) {
     console.error(err)
   }
-  console.data(info)
+  console.info(data)
+  console.info(data.toString()) //to see text
+})
+
+//writing files
+fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
+  console.info(`files written`)
+})
+
+//node will create the file automatically
+fs.writeFile('./docs/blog2.txt', 'hello, world', () => {
+  console.info(`files written`)
 })
