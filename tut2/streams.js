b9c0480460dd21e9bce2285adf743ba9f9a09a86
russdb <drussell81+github@gmailcom>
2023-02-25 13:29:03 -0800

    reading and writing streams

--- tut2/streams.js
+++ tut2/streams.js
@@ -2,9 +2,11 @@ const fs = require('fs')
 
 //the encoding is optional
 const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8' })
-
+const writeStream = fs.createWriteStream('./docs/blog4.txt')
 //on data event listener
 readStream.on('data', (chunk) => {
-  console.info(`---NEW CHUNK---`)
+  console.info(`\n------------------READING NEW CHUNK------------------\n`)
   console.info(chunk)
+  console.info(`\n------------------WRITING NEW CHUNK------------------\n`)
+  writeStream.write(chunk)
 })
