492ec67776f4f392fc60ba604b3cba2bacd61aff
russdb <drussell81+github@gmailcom>
2023-02-25 15:31:08 -0800

    fold basic response types into request switch

--- tut4/server.js
+++ tut4/server.js
@@ -19,11 +19,14 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       path += 'index.html'
+      res.statusCode = 200
       break;
     case '/about':
       path += 'about.html'
+      res.statusCode = 200
       break;
     default: path += '404.html'
+      res.statusCode = 404
       break;
   }
   //read the file 
