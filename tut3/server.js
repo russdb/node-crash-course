97c33dff2c61dd6f5595ef420a1227893e86fc72
russdb <drussell81+github@gmailcom>
2023-02-25 14:55:23 -0800

    ?

--- tut3/server.js
+++ tut3/server.js
@@ -1,11 +1,11 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-console.info(`request made`)
+  console.info(`request made`)
 })
 
 const port = 3000
 server.listen(port, 'localhost', () => {
   console.info(`listening on port ${port}`)
 })
-
-//hit ctrl + c in termianl to end process
+//this code is running on the server, so it won't show in console on the browser
+//hit ctrl + c in termianl to end process git
