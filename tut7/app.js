e527c7f2f07291188d46a0687789f5bcbccdee48
russdb <drussell81+github@gmailcom>
2023-02-25 16:53:14 -0800

    added tut7 from tut6 files

---
+++ tut7/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+//creat an express app
+const app = express()
+
+//now listen for requests
+app.listen(3000)
+
+//respond to requests
+app.get('/', (req,res) => {
+  //express automatically does the header and status code
+  res.sendFile('./views/index.html', {root: __dirname }) //relative path, so set the root
+})
+app.get('/about', (req,res) => {
+  //express automatically does the header and status code
+  res.sendFile('./views/about.html', {root: __dirname }) //relative path, so set the root
+})
+//redirects
+app.get('/about-us', (req,res) => {
+  res.redirect('/about')
+})
+//404 page, if no match with get() above, this will fire
+app.use((req,res) => {
+  //can set status at same time
+  res.status(404).sendFile('./views/404.html', {root:__dirname})
+})
+//now do nodemon app
