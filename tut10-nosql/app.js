3455928d1a6d9dbb79a242fc376ca4ae8bd7820a
russdb <drussell81+github@gmailcom>
2023-02-26 14:31:01 -0800

    adding tut10-nosql from tut9-nosql files

---
+++ tut10-nosql/app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+//creat an express app
+const app = express()
+//connect to mongodb
+let dbUser = 'russd',
+    dbpw = 'qmd9RXlUiL6627Hz',
+    dbName = 'node-tuts'
+const dbURI = `mongodb+srv://${dbUser}:${dbpw}@nodetuts.dluumlr.mongodb.net/${dbName}?retryWrites=true&w=majority`
+mongoose.connect(dbURI) //async
+  .then((result) => { 
+    console.info('connected to db')
+    app.listen(3000)
+  })
+  .catch((err) => { console.error(err)} )
+
+// register view engine with express
+app.set('view engine', 'ejs')
+
+
+// middleware & static files
+app.use(express.static('public'))
+
+//add middleware
+app.use(morgan('dev'))
+//middleware must go above .get(), since it sends the response back on a match and it stops reading the file
+
+
+//respond to requests & route
+app.get('/', (req,res) => {
+  res.redirect('/blogs')
+})
+
+app.get('/about', (req,res) => {
+  res.render('about', {title: `About Us`})
+})
+
+//blog routes
+app.get('/blogs', function(req,res){
+  Blog.find().sort({ createdAt: -1 }) //newest first
+    .then( (result) => { res.render('index',{title: 'All Blogs', blogs: result}) })
+    .catch((err) => {console.warn(err)})
+})
+//create blog post
+app.get('/blogs/create', (req,res) => {
+  res.render('create', {title: 'Create'})
+})
+//404 page, if no match with get() above, this will fire
+app.use((req,res) => {
+  res.status(404).render('404', {title: '404'})
+})
+//now do nodemon app
---
+++ tut10-nosql/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  snippet: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+
+
+const Blog = mongoose.model('Blog', blogSchema) //name is important, adds an 's' at the end
+module.exports = Blog
+
