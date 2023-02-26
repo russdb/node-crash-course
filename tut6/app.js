9e85d04129e587800829827a0fd70f65a433b978
russdb <drussell81+github@gmailcom>
2023-02-25 16:48:36 -0800

    add status code to 404, clean up file

--- tut6/app.js
+++ tut6/app.js
@@ -9,14 +9,11 @@ app.listen(3000)
 //respond to requests
 app.get('/', (req,res) => {
   //express automatically does the header and status code
-  // res.send('<p>home page</p>')
   res.sendFile('./views/index.html', {root: __dirname }) //relative path, so set the root
 })
 app.get('/about', (req,res) => {
   //express automatically does the header and status code
-  // res.send('<p>about page</p>') 
   res.sendFile('./views/about.html', {root: __dirname }) //relative path, so set the root
-
 })
 
 //redirects
@@ -26,6 +23,7 @@ app.get('/about-us', (req,res) => {
 
 //404 page, if no match with get() above, this will fire
 app.use((req,res) => {
-  res.sendFile('./views/404.html', {root:__dirname})
+  //can set status at same time
+  res.status(404).sendFile('./views/404.html', {root:__dirname})
 })
 //now do nodemon app
