8491769219a227767e5f735c018fea9a3ef1307c
russdb <drussell81+github@gmailcom>
2023-02-25 16:40:19 -0800

    add routing to html pages with express

--- tut6/app.js
+++ tut6/app.js
@@ -9,12 +9,14 @@ app.listen(3000)
 //respond to requests
 app.get('/', (req,res) => {
   //express automatically does the header and status code
-  res.send('<p>home page</p>') 
+  // res.send('<p>home page</p>')
+  res.sendFile('./views/index.html', {root: __dirname }) //relative path, so set the root
 })
 
 app.get('/about', (req,res) => {
   //express automatically does the header and status code
-  res.send('<p>about page</p>') 
-})
+  // res.send('<p>about page</p>') 
+  res.sendFile('./views/about.html', {root: __dirname }) //relative path, so set the root
 
+})
 //now do nodemon app
