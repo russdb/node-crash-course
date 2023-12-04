32092af59a7f2fe78980d4da4733c21e5419c513
russdb <drussell81+github@gmailcom>
2023-12-03 22:20:43 -0800

    rewatch and add notes

--- tut7/app.js
+++ tut7/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 // register view engine with express
+// default folder is ./views
 app.set('view engine', 'ejs')
 
 //now listen for requests
@@ -11,15 +12,15 @@ app.listen(3000)
 
 //respond to requests
 app.get('/', (req,res) => {
-  const blogs = [
+  const blogs = [ //the data to send to the page as the blog
     {title: 'This is a blog title', snippet: 'The man lowered his sunglasses and peered through the small opening of the keyhole. Inside, he saw a man sitting on the bed.'},
     {title: 'This is a blog title', snippet: 'The man lowered his sunglasses and peered through the small opening of the keyhole. Inside, he saw a man sitting on the bed.'},
     {title: 'This is a blog title', snippet: 'The man lowered his sunglasses and peered through the small opening of the keyhole. Inside, he saw a man sitting on the bed.'},
   ]
-  res.render('index', {title: 'Home', blogs}) //can also be blogs: blog, but not needed if same name.
+  res.render('index', {title: 'Home', blogs}) //use second parameter to send data. can also be blogs: blog, but not needed if same name.
 })
 app.get('/about', (req,res) => {
-  res.render('about', {title: `About Us`})
+  res.render('about', {title: `About Us`}) //pass data with an object as the second parameter
 })
 //create blog post
 app.get('/blogs/create', (req,res) => {
